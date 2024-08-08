3f3856c9f8e355d6f8cd7a47662d4c475dc67665
jiyapalod <jiyapalod@gmail.com>
2024-08-08 11:12:01 +0530

    Add files via upload

---
+++ integer.cpp
@@ -0,0 +1,29 @@
+
+//Jiya Palod
+//23070123070
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    
+    int a;
+    cout<<"Enter  number:\n";
+    cin>>a;
+    if(a>=0)
+        {
+            cout<<"Positive number";
+        }
+    else
+        {
+            cout<<"Negative number";
+        }
+}
+
+/*OUTPUT:
+
+Enter a number: 5
+Positive number.
+
+Enter a number: -6
+Negative number.*/
